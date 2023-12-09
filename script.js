d08249ab112176aebccdecebf225c328bca13ea7
Emanoel Timbó <emanoelpontes8@gmail.com>
2023-12-08 21:23:50 -0300

    Implementado a parte de codificação do carrinho em  JS

--- script.js
+++ script.js
@@ -115,3 +115,76 @@ window.addEventListener("DOMContentLoaded", () => {
   });
   firstControl.classList.add("active-testimonial");
 });
+
+//Manipulação do carrinho de produtos
+const productsArray = [];
+
+function increaseQuantity(event) {
+  const quatityElement =
+    event.target.parentElement.querySelector(".number-quantity");
+  const quantity = parseInt(quatityElement.textContent); //valor dentro do .number-quantity e convertendo p int
+  quatityElement.textContent = quantity + 1;
+}
+
+function decreaseQuantity(event) {
+  const quatityElement =
+    event.target.parentElement.querySelector(".number-quantity");
+  const quantity = parseInt(quatityElement.textContent);
+
+  if (quantity > 0) {
+    quatityElement.textContent = quantity - 1;
+  }
+}
+
+function updateCards() {
+  const cart = document.querySelector(".item-cart");
+  cart.textContent = productsArray.length;
+}
+
+function addProductTocart(event) {
+  const productCard = event.target.closest(".card-new-products"); //antecesor mais perto onde está o click
+  const productName = productCard.querySelector(".info-product h3").textContent;
+  const priceText = productCard.querySelector(".new-price").textContent;
+  const price = parseFloat(priceText.replace("R$", ""));
+
+  const quantityElement = productCard.querySelector(".number-quantity");
+
+  let quantity = parseInt(quantityElement.textContent);
+
+  const existingProductIndex = productsArray.findIndex(
+    (product) => product.productName === productName
+  );
+
+  if (quantity > 0) {
+    if (existingProductIndex !== -1) {
+      productsArray[existingProductIndex].quantity - quantity;
+    } else {
+      productsArray.push({
+        productName: productName,
+        price: price,
+        quantity: quantity,
+      });
+    }
+  } else {
+    if (existingProductIndex !== -1) {
+      productsArray.splice(existingProductIndex, 1);
+    }
+  }
+  updateCards();
+}
+
+const increaseButtons = document.querySelectorAll(".increase-quantity");
+const decreaseButtons = document.querySelectorAll(".decrease-quantity");
+const addCardButtons = document.querySelectorAll(".confirm-add-cart");
+
+increaseButtons.forEach((button) => {
+  button.addEventListener("click", increaseQuantity);
+});
+
+decreaseButtons.forEach((button) => {
+  button.addEventListener("click", decreaseQuantity);
+});
+
+addCardButtons.forEach((button) => {
+  button.addEventListener("click", addProductTocart);
+});
